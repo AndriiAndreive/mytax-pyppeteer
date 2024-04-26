aa8044f1f1bffdc46e34d5c56d8ea7556d7b9d41
Andrii Andreiev <andreievandrii71@gmail.com>
2024-04-26 06:55:40 +0300

    commented on the wrong code

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ from email_handler import EmailHandler
 import traceback
 import os
 load_dotenv('.env')
-os.chmod('./element_screenshot.png', 0o755)
-os.chmod('./screenshot.png', 0o755)
 
 class Account(BaseModel):
     email: str = Field(..., description="recipient email")
@@ -44,55 +42,57 @@ async def get_status(account: Account):
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--remote-debugging-port=9222")
     screenshot_path = 'taxstatus.png'
+    
     try:
         driver = webdriver.Chrome(options=chrome_options)
         driver.get('https://mytax.dc.gov/_/')
-
-        # Find the textbox by classname
-        link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#l_Df-1-15 span.ColIconText')))
-        link.click()
-
-        # Wait for the first textbox to be visible and enabled
-        textbox1 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'Dc-a')))
-        textbox1.send_keys(account.password)
-
-        # Wait for the second textbox to be visible and enabled
-        textbox2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'Dc-8')))
-        textbox2.send_keys(account.name)
-
-
-        # Wait for the button to be clickable
-        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'Dc-c')))
-        button.click()
-
-        # Wait for the taxstatus element to be visible
-        taxstatus = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'caption2_Dc-j')))
-
-        # Scroll down the page using JavaScript
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
         driver.save_screenshot(screenshot_path)
 
-        # Get the location and size of the element
-        location = taxstatus.location_once_scrolled_into_view
-        size = taxstatus.size
+        # # Find the textbox by classname
+        # link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#l_Df-1-15 span.ColIconText')))
+        # link.click()
 
-        # Take a screenshot of the element
-        screenshot = driver.get_screenshot_as_png()
+        # # Wait for the first textbox to be visible and enabled
+        # textbox1 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'Dc-a')))
+        # textbox1.send_keys(account.password)
 
-        # Calculate the coordinates for cropping the screenshot
-        left = location['x']
-        top = location['y']
-        right = location['x'] + size['width']
-        bottom = location['y'] + size['height']
+        # # Wait for the second textbox to be visible and enabled
+        # textbox2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'Dc-8')))
+        # textbox2.send_keys(account.name)
 
-        image = Image.open(BytesIO(screenshot))
-        element_screenshot = image.crop((left, top, right, bottom))
 
-        # Save the cropped screenshot to a file
-        element_screenshot.save('element_screenshot.png')
+        # # Wait for the button to be clickable
+        # button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'Dc-c')))
+        # button.click()
 
-        hasSent = await EmailHandler().send_email(account.email, 'element_screenshot.png') 
+        # # Wait for the taxstatus element to be visible
+        # taxstatus = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'caption2_Dc-j')))
+
+        # # Scroll down the page using JavaScript
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # driver.save_screenshot(screenshot_path)
+
+        # # Get the location and size of the element
+        # location = taxstatus.location_once_scrolled_into_view
+        # size = taxstatus.size
+
+        # # Take a screenshot of the element
+        # screenshot = driver.get_screenshot_as_png()
+
+        # # Calculate the coordinates for cropping the screenshot
+        # left = location['x']
+        # top = location['y']
+        # right = location['x'] + size['width']
+        # bottom = location['y'] + size['height']
+
+        # image = Image.open(BytesIO(screenshot))
+        # element_screenshot = image.crop((left, top, right, bottom))
+
+        # # Save the cropped screenshot to a file
+        # element_screenshot.save('element_screenshot.png')
+
+        hasSent = await EmailHandler().send_email(account.email, screenshot_path) 
         if hasSent == True:
             return {"message": "You has been sent the email successfully."}
         else:
